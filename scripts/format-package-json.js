fd3ed2926b54cd6a92b403d1e5aedbcffcf2c32b
Joseph D. Harvey <jdharvey@us.ibm.com>
2024-01-16 14:12:38 -0500

    docs: update copyrights

--- scripts/format-package-json.js
+++ scripts/format-package-json.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 /*
- * Copyright IBM Corp. 2023, 2023
+ * Copyright IBM Corp. 2023, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
