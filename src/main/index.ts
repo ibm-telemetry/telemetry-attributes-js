541091f1839edb57549b5fac87209bf1e94840c6
Joseph D. Harvey <jdharvey@us.ibm.com>
2024-01-16 15:58:19 -0500

    feat: first pass at attribute consolidation

---
+++ src/main/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright IBM Corp. 2024, 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+export * from './custom-resource-attributes.js'
+export * from './jsx-scope-attributes.js'
+export * from './npm-scope-attributes.js'
