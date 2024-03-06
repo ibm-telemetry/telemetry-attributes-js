99be35f74c71144150acb0389758cae2e46525df
Francine Lucca <francinelucca@hotmail.com>
2024-03-06 17:25:14 -0500

    feat: add js scope function and token attributes

---
+++ src/main/js-scope-attributes.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright IBM Corp. 2023, 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+export const JsxScopeAttributes = Object.freeze({
+  //
+  // JS scope metrics
+  //
+  JS_TOKEN_METRIC: 'js.token',
+  JS_FUNCTION_METRIC: 'js.function',
+
+  //
+  // Attributes relating to a js token
+  //
+  TOKEN_NAME: 'js.token.name',
+  TOKEN_ACCESS_PATH: 'js.token.accessPath',
+
+  //
+  // Attributes relating to a js function
+  //
+  FUNCTION_NAME: 'js.function.name',
+  FUNCTION_ACCESS_PATH: 'js.function.accessPath',
+  FUNCTION_ARGUMENT_VALUESt: 'js.function.arguments.values'
+})
