7e3c2d4e43aab3cdb073be96e65343e6e8c78557
Francine Lucca <40550942+francinelucca@users.noreply.github.com>
2024-03-11 15:35:23 -0400

    fix: correct js scope attr object name (#11)
    * fix: correct js scope attr object name
    
    * feat: export js scope attributes through index

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,5 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 export * from './custom-resource-attributes.js'
+export * from './js-scope-attributes.js'
 export * from './jsx-scope-attributes.js'
 export * from './npm-scope-attributes.js'
