734e6d9170cc97981e9d20d2b6b2a7c84effbd46
Joe Harvey <51208233+jdharvey-ibm@users.noreply.github.com>
2024-04-18 16:53:07 -0400

    feat: add exported ALL_SCOPE_NAMES (#32)

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,7 +4,18 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-export * from './custom-resource-attributes.js'
-export * from './js-scope-attributes.js'
-export * from './jsx-scope-attributes.js'
-export * from './npm-scope-attributes.js'
+import { CustomResourceAttributes } from './custom-resource-attributes.js'
+import { JsScopeAttributes } from './js-scope-attributes.js'
+import { JsxScopeAttributes } from './jsx-scope-attributes.js'
+import { NpmScopeAttributes } from './npm-scope-attributes.js'
+
+export { CustomResourceAttributes }
+export { JsScopeAttributes }
+export { JsxScopeAttributes }
+export { NpmScopeAttributes }
+
+export const ALL_SCOPE_NAMES = [
+  JsScopeAttributes.SCOPE_NAME,
+  JsxScopeAttributes.SCOPE_NAME,
+  NpmScopeAttributes.SCOPE_NAME
+] as const
