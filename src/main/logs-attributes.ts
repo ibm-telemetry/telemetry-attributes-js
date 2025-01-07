3eee07e9ffd0bd6d26351ff90dc2d22e1eb3cdaf
Ignacio Becerra <i1becerr@gmail.com>
2025-01-07 13:42:57 -0800

    feat(attributes): add log attributes file (#56)
    * feat(attributes): add CI name attribute
    
    * Update src/main/custom-resource-attributes.ts
    
    Co-authored-by: Matt Rosno <matt.rosno@gmail.com>
    
    * fix: ran prettier
    
    * feat: add log attribute file
    
    * fix: update copyright
    
    ---------
    
    Co-authored-by: Matt Rosno <matt.rosno@gmail.com>

---
+++ src/main/logs-attributes.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright IBM Corp. 2025, 2024
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+/*
+ * Copyright IBM Corp. 2025, 2025
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+export const LogsAttributes = Object.freeze({
+  //
+  // General attributes
+  //
+  PROJECT_ID: 'project.id',
+  DATE: 'date',
+  LOGS: 'logs',
+
+  //
+  // Attributes related to details about where the emitter is running
+  //
+  ENVIRONMENT_NAME: 'environment.name',
+
+  //
+  // Attributes relating to details about the analyzed repository
+  //
+  ANALYZED_PATH: 'analyzed.path',
+  ANALYZED_HOST: 'analyzed.host',
+  ANALYZED_OWNER: 'analyzed.owner',
+  ANALYZED_OWNER_PATH: 'analyzed.ownerPath',
+  ANALYZED_REPOSITORY: 'analyzed.repository',
+  ANALYZED_COMMIT: 'analyzed.commit',
+  ANALYZED_REFS: 'analyzed.refs',
+
+  //
+  // Attributes related to the results of the scan
+  //
+  RESULTS_IS_COMPLETED: 'results.isCompleted',
+  RESULTS_TOTAL_DURATION: 'results.totalDuration',
+  RESULTS_TOTAL_PACKAGES: 'results.totalPackages'
+})
